16d7a21ccc623309058b7f3b903c4b8f87595213
JeongHaeng Cho <118785061+JOJeongHaeng@users.noreply.github.com>
2025-04-01 10:04:51 +0900

    생략은 죄악

--- main.cpp
+++ main.cpp
@@ -10,16 +10,16 @@ int main()
 	float FloatNum1 = 20.1f;
 	float FloatNum2 = 10.0f;
 
-	cout << IntNum1 + IntNum2 << endl;
-	cout << IntNum1 - IntNum2 << endl;
-	cout << IntNum1 * IntNum2 << endl;
-	cout << IntNum1 / IntNum2 << endl;
-	cout << IntNum1 % IntNum2 << endl << endl;
+	cout << (IntNum1 + IntNum2) << endl;
+	cout << (IntNum1 - IntNum2) << endl;
+	cout << (IntNum1 * IntNum2) << endl;
+	cout << (IntNum1 / IntNum2) << endl;
+	cout << (IntNum1 % IntNum2) << endl << endl;
 
-	cout << FloatNum1 + FloatNum2 << endl;
-	cout << FloatNum1 - FloatNum2 << endl;
-	cout << FloatNum1 * FloatNum2 << endl;
-	cout << FloatNum1 / FloatNum2 << endl;
+	cout << (FloatNum1 + FloatNum2) << endl;
+	cout << (FloatNum1 - FloatNum2) << endl;
+	cout << (FloatNum1 * FloatNum2) << endl;
+	cout << (FloatNum1 / FloatNum2) << endl;
 
 	return 0;
 }
