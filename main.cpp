12338f28305e391cadd7b9948159889a7b17e1f6
JeongHaeng Cho <118785061+JOJeongHaeng@users.noreply.github.com>
2025-04-01 10:22:22 +0900

    언리얼엔진은 헝가리안 표기법 안씀

--- main.cpp
+++ main.cpp
@@ -4,22 +4,22 @@ using namespace std;
 
 int main()
 {
-	int IntNum1 = 20;
-	int IntNum2 = 10;
+	int Num1 = 20;
+	int Num2 = 10;
 
-	float FloatNum1 = 20.1f;
-	float FloatNum2 = 10.0f;
+	float Num3 = 20.1f;
+	float Num4 = 10.0f;
 
-	cout << (IntNum1 + IntNum2) << endl;
-	cout << (IntNum1 - IntNum2) << endl;
-	cout << (IntNum1 * IntNum2) << endl;
-	cout << (IntNum1 / IntNum2) << endl;
-	cout << (IntNum1 % IntNum2) << endl << endl;
+	cout << (Num1 + Num2) << endl;
+	cout << (Num1 - Num2) << endl;
+	cout << (Num1 * Num2) << endl;
+	cout << (Num1 / Num2) << endl;
+	cout << (Num1 % Num2) << endl << endl;
 
-	cout << (FloatNum1 + FloatNum2) << endl;
-	cout << (FloatNum1 - FloatNum2) << endl;
-	cout << (FloatNum1 * FloatNum2) << endl;
-	cout << (FloatNum1 / FloatNum2) << endl;
+	cout << (Num3 + Num4) << endl;
+	cout << (Num3 - Num4) << endl;
+	cout << (Num3 * Num4) << endl;
+	cout << (Num3 / Num4) << endl;
 
 	return 0;
 }
