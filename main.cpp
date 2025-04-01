8947ce155291d9e14e3ce96eda7bce840dc5e36c
JeongHaeng Cho <118785061+JOJeongHaeng@users.noreply.github.com>
2025-04-01 09:11:23 +0900

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int iNum1 = 20;
+	int iNum2 = 10;
+
+	float fNum1 = 20.1f;
+	float fNum2 = 10.0f;
+
+	cout << iNum1 + iNum2 << endl;
+	cout << iNum1 - iNum2 << endl;
+	cout << iNum1 * iNum2 << endl;
+	cout << iNum1 / iNum2 << endl;
+	cout << iNum1 % iNum2 << endl << endl;
+
+	cout << fNum1 + fNum2 << endl;
+	cout << fNum1 - fNum2 << endl;
+	cout << fNum1 * fNum2 << endl;
+	cout << fNum1 / fNum2 << endl;
+
+	return 0;
+}
